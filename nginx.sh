6cba718ad5f9ec7599ebfe6bf73f283a740255e9
Manoj <manojrsadasivan@gmail.com>
2023-10-23 15:48:35 +0100

    Create nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Update apt cache.
+sudo apt-get update
+# Install Nginx.
+sudo apt-get install -y nginx
+# Set the home page.
+echo "<html><body><h2>This is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
