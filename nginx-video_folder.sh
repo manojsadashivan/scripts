165d6eaaa5b6b192af05671e592dbab543a2af80
Manoj <manojrsadasivan@gmail.com>
2023-12-10 11:55:38 +0000

    Add files via upload

---
+++ nginx-video_folder.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Update apt cache.
+sudo apt-get update
+# Install Nginx.
+sudo apt-get install -y nginx
+#Create new folder
+sudo mkdir /var/www/html/video
+# Set the home page.
+echo "<html><body><h2>This is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/video/index.html
